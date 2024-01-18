bb97828f834343e8d75fe75da168f362a65a6a5c
Dave Judge <143704883+lvl7wizard@users.noreply.github.com>
2024-01-18 11:32:35 +0000

    refactor get /api/articles/:article_id endpoint to add comment_count property

--- models/news.models.js
+++ models/news.models.js
@@ -12,7 +12,14 @@ exports.fetchEndpoints = async () => {
 }
 
 exports.fetchArticleById = async (article_id) => {
-    const article = await db.query('SELECT * FROM articles WHERE article_id = $1', [article_id])
+    const article = await db.query(`
+    SELECT articles.article_id, articles.title, articles.topic, articles.author, articles.created_at, articles.votes, articles.article_img_url, articles.body, 
+    COALESCE(CAST(COUNT(comments.article_id) AS INTEGER), 0) AS comment_count 
+    FROM articles 
+    LEFT JOIN comments on comments.article_id = articles.article_id 
+    WHERE articles.article_id = $1 
+    GROUP BY articles.article_id;
+    `, [article_id])
     if (article.rows.length !== 1) {
         return Promise.reject({ status: 404, msg: 'Not Found - There are no articles with that article_id number' });
     }
