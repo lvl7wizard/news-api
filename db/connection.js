3d4092e6706c613a61fac6708fbe1151524dd0d0
Dave Judge <143704883+lvl7wizard@users.noreply.github.com>
2024-01-18 15:03:17 +0000

    adds husky to dependencies for api hosting

--- db/connection.js
+++ db/connection.js
@@ -3,8 +3,9 @@ const { Pool } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
 
 require('dotenv').config({
-  path: `${__dirname}/../.env.${ENV}`,
+  path: `${__dirname}/../.env.${ENV}`
 });
+
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE_URL not set');
