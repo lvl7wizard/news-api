b8614b4cf9692d48fff2336385ec92d646ec0a63
Dave Judge <143704883+lvl7wizard@users.noreply.github.com>
2024-02-05 09:54:13 +0000

    add CORS package to allow Cross Origin Resource Sharing

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const apiRouter = require('./routes/api-router');
 const { handleInvalidEndpoints, handleCustomErrors, handlePsqlErrors, handleServerErrors } = require('./errors/index');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use('/api', apiRouter);
