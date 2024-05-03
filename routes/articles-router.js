001b3f3a7c83048424dd19d4f7e93cd7f49cfb0b
Dave Judge <143704883+lvl7wizard@users.noreply.github.com>
2024-05-03 10:17:34 +0100

    fix deleteArticle bug

--- routes/articles-router.js
+++ routes/articles-router.js
@@ -1,6 +1,5 @@
 const articlesRouter = require('express').Router();
-const { getArticles, getArticlesById, getCommentsByArticleId, postComment, patchArticle, postArticle } = require('../controllers/news.controllers');
-const { removeArticleById } = require('../models/news.models');
+const { getArticles, getArticlesById, getCommentsByArticleId, postComment, patchArticle, postArticle, deleteArticle } = require('../controllers/news.controllers');
 
 articlesRouter.route('/')
 .get(getArticles)
@@ -8,7 +7,7 @@ articlesRouter.route('/')
 articlesRouter.route('/:article_id')
 .get(getArticlesById)
 .patch(patchArticle)
-.delete(removeArticleById);
+.delete(deleteArticle);
 articlesRouter.route('/:article_id/comments')
 .get(getCommentsByArticleId)
 .post(postComment);
